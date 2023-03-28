5bfaab71740bf11cccc7696ea41d14bb86a885b4
akira <84250791+akira0816@users.noreply.github.com>
2023-03-29 03:18:05 +0900

    Install @expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
-import MemoDetailScreen from "./src/screens/MemoDetailScreen";
-// import MemoListScreen from "./src/screens/MemoListScreen";
+// import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+import MemoListScreen from "./src/screens/MemoListScreen";
 
 export default function App() {
-  return <MemoDetailScreen />;
+  return <MemoListScreen />;
 }
