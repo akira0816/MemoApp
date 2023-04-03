fca8e5f173afebad92ba06f75233583ef8d34113
akira <84250791+akira0816@users.noreply.github.com>
2023-04-04 03:15:58 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
-import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+// import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+// import MemoEditScreen from "./src/screens/MemoEditScreen";
 // import MemoListScreen from "./src/screens/MemoListScreen";
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    // <MemoDetailScreen />
     // <MemoListScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
